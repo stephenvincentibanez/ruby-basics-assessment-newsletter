37ced3b07b8c532e687f5fd21e8cbccb5244cc54
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-10-07 10:28:45 -0700

    Done.

--- newsletter.rb
+++ newsletter.rb
@@ -27,20 +27,26 @@ ARTICLES = [
 # Methods to generate the newsletter
 #########################
 
+
 def calculate_recipients
+  recipients = SUBSCRIBERS - UNSUBSCRIBED
+  recipients 
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+  ARTICLES.first(number_of_articles.to_i)
 end
 
 def print_recipients
+  binding.pry
+  calculate_recipients.split
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
 end
+
 
 def print_one_article(article)
   # Write a method that will take an article hash
@@ -54,7 +60,7 @@ def print_many_articles(articles)
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -80,7 +86,6 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
 end
 
 def run
